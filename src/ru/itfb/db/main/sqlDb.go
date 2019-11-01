47e3202496766388c9d9542dd74ab520d89429b6
Егоров Владимир Викторович <v.egorov@itfbgroup.ru>
2019-11-01 21:27:10 +0300

    Что это

--- src/ru/itfb/db/main/sqlDb.go
+++ src/ru/itfb/db/main/sqlDb.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+
+
 )
 
-
-type UserTest struct{
-	id int
+type UserTest struct {
+	id   int
 	name string
 }
+var db *sql.DB
+//var rows *sql.Rows
 
 func main() {
 
@@ -23,16 +30,14 @@ func main() {
 	defer db.Close()
 
 	//Устанавливаем соединение GO_Train_target
-	connStr1 := "user=egorovvv password=12345 dbname=GO_Train_target sslmode=disable"
+	/*connStr1 := "user=egorovvv password=12345 dbname=GO_Train_target sslmode=disable"
 	db_tg, err_tg := sql.Open("postgres", connStr1)
 	if err_tg != nil {
 		panic(err_tg)
 	}
-	defer db_tg.Close()
+	defer db_tg.Close()*/
 
-
-
-	rows, err := db.Query("select * from public.contact")
+	/*rows, err := db.Query("select * from public.contact")
 	if err != nil {
 		panic(err)
 	}
@@ -40,10 +45,10 @@ func main() {
 	contacts := []UserTest{}
 
 	// Добавляем с slice набор из таблицы public.contact
-	for rows.Next(){
+	for rows.Next() {
 		p := UserTest{}
 		err := rows.Scan(&p.id, &p.name)
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 			continue
 		}
@@ -51,23 +56,54 @@ func main() {
 	}
 
 	// Вывод содержимого таблицы public.contact из GO_Train_first
-	for _, p := range contacts{
+	for _, p := range contacts {
 		fmt.Println(p.id, p.name)
 	}
 
-
-	for _, p := range contacts{
+	for _, p := range contacts {
 
 		result, err := db_tg.Exec("insert into public.contact_target(id, name) VALUES ($1, $2)",
 			p.id, p.name)
-		if err != nil{
+		if err != nil {
 			panic(err)
 			continue
 		}
 
 		fmt.Println(result.RowsAffected())
 
+	}*/
+
+	r := mux.NewRouter()
+	r.HandleFunc("/books/{id}", getList).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+
+}
+
+func getList(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	rows, err := db.Query("select * from public.contact where id=$1", params["id"] )
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	contacts := []UserTest{}
+
+	// Добавляем с slice набор из таблицы public.contact
+	for rows.Next() {
+		p := UserTest{}
+		err := rows.Scan(&p.id, &p.name)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		contacts = append(contacts, p)
 	}
 
 
+	json.NewEncoder(w).Encode(contacts)
 }
